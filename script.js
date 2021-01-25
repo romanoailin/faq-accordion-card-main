46bded968178837bef7884fd3c943e87aa925b35
romanoailin <ailin.romano@hotmail.com>
2021-01-25 15:00:22 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//script.js
+//add selector
+console.log("te odio js");
+
+document.querySelectorAll(".question").forEach((question) => question.addEventListener("click", () =>{
+  
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        }
+        else{
+            document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"))
+            question.parentNode.classList.add("active")
+        }
+
+    }))
